eb6daf10cdbf7f9219371b9ef5d7518a0afd2f6d
Nirmal Besra <kznrml@gmail.com>
2026-07-21 05:37:07 +0530

    removed duplicates, improved the memory usage

--- front-end/rating-system/rating.js
+++ front-end/rating-system/rating.js
@@ -1,22 +1,19 @@
 var starcolor = ["rgb(241, 89, 89)","rgb(241, 157, 89)","rgb(241, 226, 89)","rgb(188, 241, 89)","rgb(124, 241, 89)"];
 window.onload = function(){
-    let cnode = Object.keys(document.getElementById("starsBlank").children);
-   console.log( document.getElementById("starsBlank").children.length);
-    console.log(cnode);
+    let cnode = document.querySelectorAll("#starsBlank span");
+
    cnode.forEach((x,idx)=>{
-    console.log(idx);
-    
-        document.getElementById(`star${idx+1}`).onmouseover = function(e){
+        x.onmouseenter = function(e){
             let colorVar = starcolor[idx];
-            document.getElementById(`star${idx+1}`).style.color = colorVar;
-            for(let i=idx;i>0;i--){
-                document.getElementById(`star${i}`).style.color = colorVar;
+            x.style.color = colorVar;
+            for(let i=idx;i>=0;i--){
+                cnode[i].style.color = colorVar;
             }
             }
-        document.getElementById(`star${idx+1}`).onmouseout = function(e){
-           document.getElementById(`star${idx+1}`).style.color = "darkkhaki"; 
-            for(let i=idx;i>0;i--){
-                document.getElementById(`star${i}`).style.color = "darkkhaki";
+        x.onmouseleave  = function(e){
+           x.style.color = "darkkhaki"; 
+            for(let i=idx;i>=0;i--){
+                cnode[i].style.color = "darkkhaki";
             }
         }
    })
